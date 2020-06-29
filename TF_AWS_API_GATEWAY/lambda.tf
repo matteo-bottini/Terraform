f52a868f05a0ac235b0d96b4831b5a269560d19e
mbottini <mbottini@dalet.com>
2020-06-28 20:48:41 -0500

    Remove interpolation

--- TF_AWS_API_GATEWAY/lambda.tf
+++ TF_AWS_API_GATEWAY/lambda.tf
@@ -26,13 +26,13 @@ EOF
 resource "aws_lambda_function" "bitcoin" {
   filename      = "TBD.zip"
   function_name = "bitcoinUSD"
-  role          = "${aws_iam_role.iam_for_lambda.arn}"
+  role          = aws_iam_role.lambda_role.arn
   handler       = "TBD-exports.test"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
   # source_code_hash = "${base64sha256(file("lambda_function_payload.zip"))}"
-  source_code_hash = "${filebase64sha256("TBD.zip")}"
+  source_code_hash = filebase64sha256("TBD.zip")
 
   runtime = "ruby2.7"
 }
